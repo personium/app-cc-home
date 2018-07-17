7278cf7bb36152ea36716eb39e53e2bf34c8996e
Dixon Siu <siu.dixon@jp.fujitsu.com>
2018-07-18 08:55:58 +0900

    FEAT: Improve UX of language selection screen
    Part of #182 that will abolish all pull-down select UI in HomeApp.

--- src/js/change_language.js
+++ src/js/change_language.js
@@ -11,13 +11,6 @@ addLoadStyleSheet = function (styleList) {
 function init() {
     ut.loadStyleSheet();
     ut.loadScript(function () {
-        $("#selectLng").val(i18next.language);
-        cm.createBackMenu("main.html", true);
-        $("#b-setlng-ok").on('click', function () {
-            $("#selectLng option:selected").each(function (index, option) {
-                i18next.changeLanguage($(option).val())
-                cm.moveBackahead(true);
-            });
-        });
+        $('#language-list li.' + i18next.language).addClass('check-mark-left');
     });
 }
